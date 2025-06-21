af6e05b2c8836319de8e984a020b2dca45855356
notabird <notabird07@gmail.com>
2025-06-20 17:38:32 -0700

    a

--- NoAmbience/Plugin Info.cs
+++ NoAmbience/Plugin Info.cs
@@ -1,5 +1,5 @@
 namespace NoAmbience;
 public class Plugin_Info
 {
-    public const string GUID = "NotABird.No.Ambience", NAME = "No Ambience", VERSION = "1.0.0";
+    public const string GUID = "NotABird.No.Ambience", NAME = "No Ambience", VERSION = "1.0.0.1";
 }
--- NoAmbience/TimeOfDayDependentAudioPatch.cs
+++ NoAmbience/TimeOfDayDependentAudioPatch.cs
@@ -8,6 +8,8 @@ public class TimeOfDayDependentAudioPatch
     [HarmonyPatch("OnEnable"), HarmonyPrefix]
     public static bool OnEnablePatch(TimeOfDayDependentAudio __instance)
     {
+        if (__instance.gameObject.name is "SoundPostForest" or "SoundPost (1)")
+            return true;
         __instance.gameObject.SetActive(false);
         return false;
     }
